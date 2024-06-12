cc018ac617a2778636f31eebcb116f5f860f3362
jinjo <creationer625@outlook.com>
2024-06-12 22:52:19 +0800

    add 2024-T3-Sample-Hanmadi/Quiz2Solution

---
+++ 2024-T3-Sample-Hanmadi/Quiz2Solution.py
@@ -0,0 +1,44 @@
+# COMP9021 24T1 - Rachid Hamadi
+# Sample Exam Question 2
+
+
+'''
+You might find the function bin() useful.
+Will be tested with n a strictly positive integer.
+'''
+
+import sys
+
+
+def f(n):
+    '''
+    >>> f(1)
+    1 in binary reads as: 1.
+    Only one bit is set to 1 in the binary representation of 1.
+    >>> f(2)
+    2 in binary reads as: 10.
+    Only one bit is set to 1 in the binary representation of 2.
+    >>> f(3)
+    3 in binary reads as: 11.
+    2 bits are set to 1 in the binary representation of 3.
+    >>> f(7)
+    7 in binary reads as: 111.
+    3 bits are set to 1 in the binary representation of 7.
+    >>> f(2314)
+    2314 in binary reads as: 100100001010.
+    4 bits are set to 1 in the binary representation of 2314.
+    >>> f(9871)
+    9871 in binary reads as: 10011010001111.
+    8 bits are set to 1 in the binary representation of 9871.
+    '''
+    str1=''
+    while n>0:
+        str1=str1+str(n%2)
+        n=n//2
+    print(str1[::-1])
+
+
+
+    # Insert your code here
+
+f(2314)
